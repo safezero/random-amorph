c63c6a1d778682da6510fcebd8f67dd9701a4802
Aakil Fernandes <aakilfernandes@gmail.com>
2017-05-10 11:21:07 -0400

    get rid of amorph dependencies/ready for faster loads

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const Amorph = require('amorph')
 const crypto = require('crypto')
 const arguguard = require('arguguard')
 
-const amorphBufferPlugin = require('amorph-buffer')
-Amorph.loadPlugin(amorphBufferPlugin)
-Amorph.ready()
-
 module.exports = function random(length) {
   arguguard('random', ['number'], arguments)
   return new Amorph(crypto.randomBytes(length), 'buffer')
--- test.js
+++ test.js
@@ -1,4 +1,11 @@
 const random = require('./')
+const Amorph = require('amorph')
+const amorphBuffer = require ('amorph-buffer')
+const amorphBn = require ('amorph-bn')
+
+Amorph.loadPlugin(amorphBuffer)
+Amorph.loadPlugin(amorphBn)
+Amorph.ready()
 
 for(var i = 0; i < 32; i ++) {
     console.log(random(i).to('hex'))
